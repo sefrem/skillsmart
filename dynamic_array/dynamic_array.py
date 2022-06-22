032a0dccaf32ca9a282d6fc393f79ff8a87bd98b
efremov.v@ati.su <ADrunkenCat3>
2022-06-22 23:57:14 +0300

    added dynamic array and insert

---
+++ dynamic_array/dynamic_array.py
@@ -0,0 +1,56 @@
+import ctypes
+
+
+class DynArray:
+
+    def __init__(self):
+        self.count = 0
+        self.capacity = 16
+        self.array = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.count
+
+    def make_array(self, new_capacity):
+        return (new_capacity * ctypes.py_object)()
+
+    def __getitem__(self, i):
+        if i < 0 or i >= self.count:
+            raise IndexError('Index is out of bounds')
+        return self.array[i]
+
+    def resize(self, new_capacity):
+        new_array = self.make_array(new_capacity)
+        for i in range(self.count):
+            new_array[i] = self.array[i]
+        self.array = new_array
+        self.capacity = new_capacity
+
+    def append(self, itm):
+        if self.count == self.capacity:
+            self.resize(2 * self.capacity)
+        self.array[self.count] = itm
+        self.count += 1
+        return self
+
+    def insert(self, i, itm):
+        if i > self.count:
+            raise IndexError('Index is out of bounds')
+        if self.count == self.capacity:
+            self.capacity *= 2
+        new_array = self.make_array(self.capacity)
+
+        for k in range(self.count+1):
+            if k == i:
+                new_array[i] = itm
+                continue
+            if k > i:
+                new_array[k] = self.array[k - 1]
+            else:
+                new_array[k] = self.array[k]
+
+        self.array = new_array
+        self.count += 1
+
+    def delete(self, i):
+        pass
---
+++ dynamic_array/tests.py
@@ -0,0 +1,47 @@
+import unittest
+
+from dynamic_array import DynArray
+
+
+class TestDynamicArray(unittest.TestCase):
+
+    def test_insert_item_capacity_not_exceeded(self):
+        expected_array = DynArray().append(1).append(2).append(4).append(3)
+        test_array = DynArray().append(1).append(2).append(3)
+
+        test_array.insert(2, 4)
+
+        for i in range(expected_array.count):
+            self.assertEqual(expected_array[i], test_array[i])
+        self.assertEqual(expected_array.capacity, test_array.capacity)
+        self.assertEqual(expected_array.count, test_array.count)
+
+    def test_insert_item_capacity_exceeded(self):
+        expected_array = DynArray().append(1).append(2).append(3).append(4).append(5).append(6) \
+            .append(7).append(8).append(9).append(100).append(10).append(11).append(12) \
+            .append(13).append(14).append(15).append(16)
+        test_array = DynArray().append(1).append(2).append(3).append(4).append(5).append(6) \
+            .append(7).append(8).append(9).append(10).append(11).append(12) \
+            .append(13).append(14).append(15).append(16)
+
+        test_array.insert(9, 100)
+
+        for i in range(expected_array.count):
+            self.assertEqual(expected_array[i], test_array[i])
+        self.assertEqual(expected_array.capacity, test_array.capacity)
+        self.assertEqual(expected_array.count, test_array.count)
+
+    def test_insert_item_as_last(self):
+        expected_array = DynArray().append(1).append(2).append(3).append(4)
+        test_array = DynArray().append(1).append(2).append(3)
+
+        test_array.insert(test_array.count, 4)
+
+        for i in range(expected_array.count):
+            self.assertEqual(expected_array[i], test_array[i])
+
+    def test_insert_in_out_of_range_position(self):
+        test_array = DynArray().append(1).append(2).append(3)
+        with self.assertRaises(IndexError):
+            test_array.insert(8, 10)
+
