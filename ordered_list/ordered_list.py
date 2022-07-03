df6722d0fa7376fc43aecf975cd7ef5c3403c3b9
Slava Efremov <fireweb2112@gmail.com>
2022-07-03 23:53:40 +0300

    added ordered linked list data structure

---
+++ ordered_list/ordered_list.py
@@ -0,0 +1,148 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+
+class OrderedList:
+    def __init__(self, asc):
+        self.head = None
+        self.tail = None
+        self.__ascending = asc
+
+    def compare(self, v1, v2):
+        if v1 < v2:
+            return -1
+        if v1 > v2:
+            return 1
+        return 0
+
+    def add(self, value):
+        item = Node(value)
+        if self.head is None:
+            self.head = item
+            item.prev = None
+            item.next = None
+            return
+        if self.tail is None:
+            self.tail = item
+            item.prev = self.head
+            item.next = None
+            self.head.next = self.tail
+            if (self.__ascending and self.compare(self.head.value, value) == 1) \
+                    or (not self.__ascending and self.compare(value, self.head.value) == 1):
+                self.tail.value = self.head.value
+                self.head.value = value
+            return
+        node = self.head
+
+        while node is not None:
+            if self.__ascending:
+                if self.compare(value, self.tail.value) == 1:
+                    item.prev = self.tail
+                    item.next = None
+                    self.tail.next = item
+                    self.tail = item
+                    break
+
+                if self.compare(node.value, value) == 1:
+                    item.prev = node.prev
+                    item.next = node
+                    if node.prev is None:
+                        self.head = item
+                    else:
+                        node.prev.next = item
+                    node.prev = item
+                    break
+            else:
+                if self.compare(value, self.head.value) == 1:
+                    item.next = self.head
+                    item.prev = None
+                    self.head.prev = item
+                    self.head = item
+                    break
+
+                if self.compare(node.value, value) == 1:
+                    item.prev = node
+                    item.next = node.next
+                    if node.next is None:
+                        self.tail = item
+                    else:
+                        node.next.prev = item
+                    node.next = item
+                    break
+
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+
+        while node is not None:
+            if (self.__ascending and node.value > val) \
+                    or (not self.__ascending and node.value < val):
+                return None
+            if node.value == val:
+                return node
+            node = node.next
+
+        return None
+
+    def delete(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                if node.prev is None and node.next is None:
+                    self.head = None
+                    self.tail = None
+                    return
+
+                if node.prev is None:
+                    self.head = node.next
+                    self.head.prev = None
+                    return
+
+                if node.next is None:
+                    node.prev.next = None
+                    self.tail = node.prev
+                    return
+
+                node.prev.next = node.next
+                node.next.prev = node.prev
+
+            node = node.next
+
+    def clean(self, asc):
+        self.__ascending = asc
+        self.head = None
+        self.tail = None
+
+    def len(self):
+        node = self.head
+        len = 0
+        while node is not None:
+            len += 1
+            node = node.next
+        return len
+
+    def get_all(self):
+        r = []
+        node = self.head
+        while node != None:
+            r.append(node)
+            node = node.next
+        return r
+
+
+class OrderedStringList(OrderedList):
+    def __init__(self, asc):
+        super(OrderedStringList, self).__init__(asc)
+
+    def compare(self, v1, v2):
+        stripped_v1 = v1.strip()
+        stripped_v2 = v2.strip()
+        if stripped_v1 < stripped_v2:
+            return -1
+        if stripped_v1 > stripped_v2:
+            return 1
+        return 0
---
+++ ordered_list/tests.py
@@ -0,0 +1,188 @@
+import unittest
+
+from ordered_list import OrderedList, OrderedStringList
+
+
+def get_list_values(list):
+    values = []
+    node = list.head
+    while node is not None:
+        values.append((node.prev.value if node.prev else None, node.value))
+        if node.next is None:
+            values.append((node.value, None))
+        node = node.next
+
+    return values
+
+
+class TestOrderedList(unittest.TestCase):
+
+    def assert_lists_equality(self, list1, list2):
+        self.assertEqual(get_list_values(list1), get_list_values(list2))
+
+    def test_adding_item_to_empty_list(self):
+        ordered_list = OrderedList(True)
+        ordered_list.add(1)
+
+        self.assertEqual([(None, 1), (1, None)], get_list_values(ordered_list))
+        self.assertEqual(1, ordered_list.head.value)
+        self.assertEqual(None, ordered_list.tail)
+
+    def test_adding_2_items_to_empty_list(self):
+        ordered_list = OrderedList(True)
+        ordered_list.add(1)
+        ordered_list.add(2)
+
+        self.assertEqual([(None, 1), (1, 2), (2, None)], get_list_values(ordered_list))
+        self.assertEqual(1, ordered_list.head.value)
+        self.assertEqual(2, ordered_list.tail.value)
+
+    def test_adding_2_items_to_empty_asc_list_second_lower(self):
+        ordered_list = OrderedList(True)
+        ordered_list.add(2)
+        ordered_list.add(1)
+
+        self.assertEqual([(None, 1), (1, 2), (2, None)], get_list_values(ordered_list))
+        self.assertEqual(1, ordered_list.head.value)
+        self.assertEqual(2, ordered_list.tail.value)
+
+    def test_adding_2_items_to_empty_desc_list_second_greater(self):
+        ordered_list = OrderedList(False)
+        ordered_list.add(2)
+        ordered_list.add(1)
+
+        self.assertEqual([(None, 2), (2, 1), (1, None)], get_list_values(ordered_list))
+        self.assertEqual(2, ordered_list.head.value)
+        self.assertEqual(1, ordered_list.tail.value)
+
+    def test_adding_item_asc_order(self):
+        ordered_list = OrderedList(True)
+        ordered_list.add(1)
+        ordered_list.add(2)
+        ordered_list.add(4)
+        ordered_list.add(3)
+
+        self.assertEqual([(None, 1), (1, 2), (2, 3), (3, 4), (4, None)], get_list_values(ordered_list))
+        self.assertEqual(1, ordered_list.head.value)
+        self.assertEqual(4, ordered_list.tail.value)
+
+    def test_adding_item_desc_order(self):
+        ordered_list = OrderedList(False)
+        ordered_list.add(1)
+        ordered_list.add(4)
+        ordered_list.add(2)
+
+        self.assertEqual([(None, 4), (4, 2), (2, 1), (1, None)], get_list_values(ordered_list))
+        self.assertEqual(4, ordered_list.head.value)
+        self.assertEqual(1, ordered_list.tail.value)
+
+    def test_adding_same_value_asc_order(self):
+        ordered_list = OrderedList(True)
+        ordered_list.add(4)
+        ordered_list.add(2)
+        ordered_list.add(2)
+        ordered_list.add(1)
+
+        self.assertEqual([(None, 1), (1, 2), (2, 2), (2, 4), (4, None)], get_list_values(ordered_list))
+        self.assertEqual(1, ordered_list.head.value)
+        self.assertEqual(4, ordered_list.tail.value)
+
+    def test_adding_few_values_desc_order(self):
+        ordered_list = OrderedList(False)
+        ordered_list.add(2)
+        ordered_list.add(1)
+        ordered_list.add(4)
+        ordered_list.add(3)
+        ordered_list.add(90)
+
+        self.assertEqual([(None, 90), (90, 4), (4, 3), (3, 2), (2, 1), (1, None)], get_list_values(ordered_list))
+        self.assertEqual(90, ordered_list.head.value)
+        self.assertEqual(1, ordered_list.tail.value)
+
+    def test_find_element_by_value(self):
+        ordered_list = OrderedList(True)
+        ordered_list.add(2)
+        ordered_list.add(1)
+        ordered_list.add(4)
+        ordered_list.add(3)
+        ordered_list.add(90)
+
+        self.assertEqual(4, ordered_list.find(4).value)
+
+    def test_not_found_element(self):
+        ordered_list = OrderedList(True)
+        ordered_list.add(2)
+        ordered_list.add(1)
+        ordered_list.add(4)
+
+        self.assertEqual(None, ordered_list.find(24))
+
+    def test_deleting_element(self):
+        ordered_list = OrderedList(True)
+        ordered_list.add(1)
+        ordered_list.add(4)
+        ordered_list.add(2)
+
+        ordered_list.delete(4)
+
+        self.assertEqual([(None, 1), (1, 2), (2, None)], get_list_values(ordered_list))
+        self.assertEqual(1, ordered_list.head.value)
+        self.assertEqual(2, ordered_list.tail.value)
+
+    def test_deleting_element_from_empty_list(self):
+        ordered_list = OrderedList(True)
+
+        self.assertEqual(None, ordered_list.delete(23))
+
+    def test_deleting_two_first_elements(self):
+        ordered_list = OrderedList(False)
+        ordered_list.add(1)
+        ordered_list.add(4)
+        ordered_list.add(2)
+
+        ordered_list.delete(4)
+        ordered_list.delete(2)
+
+        self.assertEqual([(None, 1), (1, None)], get_list_values(ordered_list))
+        self.assertEqual(1, ordered_list.head.value)
+        self.assertEqual(1, ordered_list.tail.value)
+
+    def test_clean(self):
+        ordered_list = OrderedList(False)
+        ordered_list.add(1)
+        ordered_list.add(4)
+        ordered_list.add(2)
+
+        ordered_list.clean(True)
+
+        self.assertEqual([], get_list_values(ordered_list))
+
+    def test_get_length(self):
+        ordered_list = OrderedList(False)
+        ordered_list.add(1)
+        ordered_list.add(4)
+
+        self.assertEqual(2, ordered_list.len())
+
+
+class TestOrderedListString(unittest.TestCase):
+
+    def test_adding_asc_order(self):
+        ordered_list = OrderedStringList(True)
+        ordered_list.add('c')
+        ordered_list.add(' ba ')
+        ordered_list.add('a')
+
+        self.assertEqual([(None, 'a'), ('a', ' ba '), (' ba ', 'c'), ('c', None)], get_list_values(ordered_list))
+        self.assertEqual('a', ordered_list.head.value)
+        self.assertEqual('c', ordered_list.tail.value)
+
+    def test_adding_desc_order(self):
+        ordered_list = OrderedStringList(False)
+        ordered_list.add('g')
+        ordered_list.add(' wx')
+        ordered_list.add('z')
+
+        self.assertEqual([(None, 'z'), ('z', ' wx'), (' wx', 'g'), ('g', None)], get_list_values(ordered_list))
+        self.assertEqual('z', ordered_list.head.value)
+        self.assertEqual('g', ordered_list.tail.value)
