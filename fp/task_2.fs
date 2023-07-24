0ade11d5c8ac420d16b12686c5aedecdbb8e8eca
Slava Efremov <efremov.v@ati.su>
2023-07-24 23:11:43 +0200

    upd task 2

--- fp/task_2.fs
+++ fp/task_2.fs
@@ -1,6 +1,7 @@
-let g n = n + 5  // int
+let g n = n + 5  // int -> int
 
 let gg = fun n -> n + 5 // int -> int
 
-let h (x: float, y: float) = System.Math.Sqrt(x**2 + y**2) // float * float -> float
+let h (x, y) = System.Math.Sqrt(x*x + y*y) // float * float -> float
+
 
