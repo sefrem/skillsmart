63440d4fcd105b88e1585ff430e9e307b38b6f23
Slava Efremov <efremov.v@ati.su>
2023-08-15 16:31:46 +0200

    added task 12

---
+++ fp/task_12.fs
@@ -0,0 +1,20 @@
+
+let rec iter = function
+            | a, n when List.length a < n -> iter((List.length a)+1 :: a, n)
+            | a, _ -> a
+
+let rec iterEven = function
+            | a, n, start when List.length a < n && start % 2 = 0 -> iterEven(start :: a, n, start+2)
+            | a,_, _ -> a
+
+// 34.1
+let rec upto = function        
+    | n -> List.rev (iter([], n))
+
+// 34.2
+let rec dnto = function        
+    | n -> iter([], n)
+
+// 34.3
+let rec evenn = function
+     n -> List.rev (iterEven([], n, 0))
