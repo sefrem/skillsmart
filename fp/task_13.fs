803744369e966555c6943a58aefd62839cfd2e36
sefrem <47655937+sefrem@users.noreply.github.com>
2023-08-18 17:14:11 +0200

    Update task_13.fs

--- fp/task_13.fs
+++ fp/task_13.fs
@@ -1,9 +1,29 @@
-
+// 39.1
 let rec rmodd = function
     | [] -> []
     | [x] -> [x]
     | _ :: (head :: tail) -> head :: rmodd tail
 
+// 39.2
 let rec del_even = function
     | head :: tail -> if head % 2 = 0 then del_even tail else head :: del_even tail
     | _ -> []
+
+// 39.3
+let rec multiplicity = function
+    | (x, head :: tail) -> if head = x then 1 + multiplicity(x, tail) else multiplicity(x, tail)
+    | (_, []) -> 0
+
+// 39.4
+let rec split = function
+    | head1 :: head2 :: tail -> let heada, headb = split tail 
+                                head1::heada, head2::headb
+    | [x] -> [x], []
+    | [] -> [], []
+
+// 39.5
+let rec zip = function
+    | head1 :: tail1, head2 :: tail2 -> (head1, head2) :: zip(tail1, tail2)                            
+    | _ -> []
+
+
