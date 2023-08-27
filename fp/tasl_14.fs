697e802dcfb32fc4cff13ce4103aa3339ad4874c
Slava Efremov <efremov.v@ati.su>
2023-08-27 17:17:11 +0200

    upd task 14

--- fp/tasl_14.fs
+++ fp/tasl_14.fs
@@ -3,23 +3,23 @@ let rec binary_search (x, list: list<int>, startIndex, endIndex, isInsert) =
     let index = startIndex + (endIndex - startIndex ) / 2
     match () with
     | () when startIndex > endIndex -> if isInsert then startIndex else -1
-    | () when x = list[index] -> index
-    | () when x < list[index] -> binary_search (x, list, startIndex, index-1, isInsert)
-    | () when x > list[index] -> binary_search (x, list, index+1, endIndex, isInsert)
+    | () when x = list.[index] -> index
+    | () when x < list.[index] -> binary_search (x, list, startIndex, index-1, isInsert)
+    | () when x > list.[index] -> binary_search (x, list, index+1, endIndex, isInsert)
     | () -> -1
 
 let rec find_first (list: list<int>, value, index) = 
     match () with
-    | () when index < 0 || list[index] <> value -> index+1
-    | () when list[index] = value -> find_first (list, value, index-1)
+    | () when index < 0 || list.[index] <> value -> index+1
+    | () when list.[index] = value -> find_first (list, value, index-1)
     | () -> index
 
 let countFromIndex (listA: list<int>, startIndex) =
-    let startValue = listA[startIndex]
+    let startValue = listA.[startIndex]
     let rec iter (index, acc) = 
         match () with
         | () when index = List.length listA -> acc
-        | () when listA[index] = startValue -> iter(index+1, acc+1)
+        | () when listA.[index] = startValue -> iter(index+1, acc+1)
         | () -> acc
     
     iter(startIndex, 0)
@@ -47,12 +47,12 @@ let rec insert (xs, n) =
 let rec intersect (xs1, xs2) =
     let rec iter (index, result) = 
         if index >= List.length xs1 then result else
-        let q2 = count(xs2, xs1[index])
+        let q2 = count(xs2, xs1.[index])
         let q1 = if q2 > 0 then countFromIndex(xs1, index) else 1
 
         match () with
         | () when index > List.length xs1 - 1 -> result
-        | () when q2 > 0 -> iter (index+q1, result @ List.init (q2+q1) (fun _ -> xs1[index]))
+        | () when q2 > 0 -> iter (index+q1, result @ List.init (q2+q1) (fun _ -> xs1.[index]))
         | () -> iter (index+1, result)
 
     iter (0, [])
@@ -64,8 +64,8 @@ let rec plus (xs1, xs2) =
     
     let rec iter (index1, index2, result) =   
         match () with
-        | () when (lastIndex2 < index2 && lastIndex1 >= index1) || (index1 <= lastIndex1 && xs1[index1] <= xs2[index2]) -> iter(index1+1, index2, result @ [xs1[index1]])
-        | () when (lastIndex1 < index1 && lastIndex2 >= index2) || (index2 <= lastIndex2 && xs1[index1] > xs2[index2] )-> iter(index1, index2+1, result @ [xs2[index2]])
+        | () when (lastIndex2 < index2 && lastIndex1 >= index1) || (index1 <= lastIndex1 && xs1.[index1] <= xs2.[index2]) -> iter(index1+1, index2, result @ [xs1.[index1]])
+        | () when (lastIndex1 < index1 && lastIndex2 >= index2) || (index2 <= lastIndex2 && xs1.[index1] > xs2.[index2] )-> iter(index1, index2+1, result @ [xs2.[index2]])
         | () when lastIndex1 < index1 && lastIndex2 < index2 -> result
         | () -> result
 
@@ -76,8 +76,8 @@ let rec minus (xs1, xs2) =
     let rec iter(index, result) = 
         match () with
         | () when index = List.length xs1 -> result
-        | () when binary_search(xs1[index], xs2, 0, List.length xs2-1, false) <> -1 -> iter(index+1, result)
-        | () -> iter(index+1, result @ [xs1[index]])
+        | () when binary_search(xs1.[index], xs2, 0, List.length xs2-1, false) <> -1 -> iter(index+1, result)
+        | () -> iter(index+1, result @ [xs1.[index]])
     
     iter(0, [])
 
@@ -86,18 +86,18 @@ let rec smallest (xs) =
     let rec iter(index, smallest) = 
         match () with
         | () when index = List.length xs -> Some smallest
-        | () when xs[index] < smallest -> iter(index+1, xs[index])
+        | () when xs.[index] < smallest -> iter(index+1, xs.[index])
         | () -> iter(index+1, smallest)
     
-    iter(0, xs[0])
+    iter(0, xs.[0])
 
 // 40.3.2
 let rec delete (n, xs) = 
     let rec iter(index, result, isRemoved) = 
         match () with
         | () when index = List.length xs -> result
-        | () when xs[index] <> n -> iter(index+1, result@[xs[index]], isRemoved)
-        | () when xs[index] = n -> if isRemoved then iter(index+1, result@[xs[index]], isRemoved) else iter(index+1, result, true)
+        | () when xs.[index] <> n -> iter(index+1, result@[xs.[index]], isRemoved)
+        | () when xs.[index] = n -> if isRemoved then iter(index+1, result@[xs.[index]], isRemoved) else iter(index+1, result, true)
         | () -> result
 
     iter(0, [], false)
