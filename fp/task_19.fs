d16ddf8fe7d460bc77fdc29017661630d23d4f4e
Slava Efremov <efremov.v@ati.su>
2023-09-10 23:39:26 +0200

    added task 19

---
+++ fp/task_19.fs
@@ -0,0 +1,18 @@
+// 48.4.1
+let rec fibo1 n n1 n2 =
+    let rec f x a b =
+        if x = 1 then a
+        else f (x-1) (a+b) a
+    f n n1 n2
+
+// 48.4.2
+let rec fibo2 n c =
+    if n <= 2 then c 1 
+    else fibo2(n-1) (fun x -> fibo2(n-2) (fun y -> c x+y))
+
+// 48.4.3
+let rec bigList n =
+    let rec f x acc = 
+        if x = 0 then acc
+        else f (x-1) (1::acc)
+    f n []
