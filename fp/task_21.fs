14e26c67c0c12b0660ecb9000277a6405d831da3
Slava Efremov <efremov.v@ati.su>
2023-09-19 23:36:23 +0200

    added task 21

---
+++ fp/task_21.fs
@@ -0,0 +1,11 @@
+// 50.2.1
+let fac_seq = seq {
+        for i in 0. ..infinity do
+        yield factorial(int i) 
+}
+
+// 50.2.2
+let seq_seq = seq {
+    for i in 0. ..infinity do
+        if int i % 2 = 0 then yield int i/2 else yield (-1) * (int i + 1)/2
+}
